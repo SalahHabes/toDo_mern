6aca7130b6f10e6d483ebf8cec6fdb5595d8a4e7
SalahHabes <salaheddinehabes@gmail.com>
2022-11-04 07:53:20 +0100

    backend: setup user; controller, model, and routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const taskRoutes = require('./routes/taskRoutes')
+const userRoutes = require('./routes/userRoutes')
 
 //express app
 const app = express()
@@ -17,6 +18,7 @@ app.use(express.json())
 
 //routes
 app.use('/api/tasks', taskRoutes)
+app.use('/api/user', userRoutes)
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
