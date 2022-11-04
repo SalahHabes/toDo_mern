31db2809abfcf7971a0baabfe40ca34ec27d41bf
SalahHabes <salaheddinehabes@gmail.com>
2022-11-04 08:45:42 +0100

    add static sign up method with validation and hash

---
+++ README.md
@@ -0,0 +1,3 @@
+MangoDB - Express js - React js - node js -
+sass - bcrypt: for hashing passwords -
+validator: for backend user info validation -
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -14,5 +16,35 @@ const userSchema = new Schema({
     }
 
 }, {timestamps: true})
+
+// static signup method
+userSchema.statics.signup = async function (email, password) {
+    
+    // validation
+    if (!email || !password) {
+        throw Error('All fields are required')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+    
+    // check for any matching emails
+    const exists = await this.findOne({ email })
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    // salt: extra characters added at the end of password before hashing to avoid password matching
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    // create user
+    const user = await this.create({ email, password: hash })
+
+    return user
+}
 
 module.exports = mongoose.model('user', userSchema)
